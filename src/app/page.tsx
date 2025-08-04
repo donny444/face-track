516cade99e3043348ac180213b7b35df42927b70
Donny <tanadornrach@gmail.com>
2025-08-04 22:48:04 +0700

    algorithm for recent attendances

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-"use client"
+"use client";
 
 // import styles from "./page.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Col, Container, Row } from "react-bootstrap";
 import { Bar } from "react-chartjs-2";
 import {
@@ -10,7 +11,7 @@ import {
   BarElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
 } from "chart.js";
 
 ChartJS.register(
@@ -24,6 +25,7 @@ ChartJS.register(
 
 import attendanceData from "@/data/attendance_data.json";
 import attendanceCounts from "@/data/attendance_counts.json";
+import recentAttendances from "@/data/recent_attendances.json";
 
 import BarChartInterface from "@/interfaces/bar_chart_interface.ts";
 import AttendanceCountsInterface from "@/interfaces/attendance_counts_interface.ts";
@@ -32,20 +34,12 @@ export default function Summary() {
   return (
     <Container>
       <Row>
-        <Col>
-          <AttendanceCountSummary countData={attendanceCounts} />
-        </Col>
-        <Col>
-          <DailyChartSummary chartData={attendanceData} />
-        </Col>
+        <AttendanceCountSummary countData={attendanceCounts} />
+        <DailyChartSummary chartData={attendanceData} />
       </Row>
       <Row>
-        <Col>
-          <AttendanceLogSummary />
-        </Col>
-        <Col>
-          <AttendeeListSummary />
-        </Col>
+        <AttendanceLogSummary />
+        <AttendeeListSummary />
       </Row>
     </Container>
   );
@@ -53,45 +47,88 @@ export default function Summary() {
 
 function AttendanceCountSummary({ countData }: AttendanceCountsInterface) {
   return (
-    <div>
+    <Col>
       <p>เข้าตรงเวลา: {countData[0].count}</p>
       <p>เข้าสาย: {countData[1].count}</p>
       <p>ขาด: {countData[2].count}</p>
-    </div>
+    </Col>
   );
 }
 
 interface DailyChartProps {
-  chartData: BarChartInterface
+  chartData: BarChartInterface;
 }
+
 function DailyChartSummary({ chartData }: DailyChartProps) {
   const options = {
     responsive: true,
     plugins: {
       title: {
         display: true,
-        text: "กราฟประจำวัน"
-      }
-    }
-  }
+        text: "กราฟประจำวัน",
+      },
+    },
+  };
 
   return (
-    <Bar options={options} data={chartData} /> 
+    <Col>
+      <Bar options={options} data={chartData} />;
+    </Col>
   );
 }
 
 function AttendanceLogSummary() {
-  return (
-    <div>
+  const startClassTime = new Date().setHours(8, 0, 0, 0);
+  const lateClassTime = new Date().setHours(10, 0, 0, 0);
+  const endClassTime = new Date().setHours(11, 0, 0, 0);
 
-    </div>
+  const attendeesLength = recentAttendances.length;
+
+  const attendanceRows = (): JSX.Element => {
+    const result: JSX.Element[] = [];
+    for (let i = attendeesLength - 1; i >= 0; i--) {
+      const attendance = recentAttendances[i];
+      const attendanceTime = new Date(attendance.datetime).getTime();
+      result[attendeesLength - i] = (
+        <tr key={i}>
+          <td>
+            {new Date(attendance.datetime).toLocaleString("th-TH", {
+              hour: "2-digit",
+              minute: "2-digit",
+              second: "2-digit",
+            })}
+          </td>
+          <td>
+            {attendance.first_name} {attendance.last_name}
+          </td>
+          {attendanceTime < lateClassTime &&
+          attendanceTime >= startClassTime ? (
+            <td className="text-success">ตรงเวลา</td>
+          ) : null}
+          {attendanceTime < endClassTime && attendanceTime >= lateClassTime ? (
+            <td className="text-warning">เข้าสาย</td>
+          ) : null}
+          {attendanceTime > endClassTime ? <td className="text-danger">ขาด</td> : null}
+        </tr>
+      );
+    }
+    return <>{result}</>;
+  };
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>เวลา</th>
+          <th>ชื่อ - สกุล</th>
+          <th>สถานะ</th>
+        </tr>
+      </thead>
+      <tbody>{attendanceRows()}</tbody>
+    </table>
   );
 }
 
 function AttendeeListSummary() {
-  return (
-    <div>
-
-    </div>
-  );
+  return <Col></Col>;
 }
