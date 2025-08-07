9e3f8280e118f035c0ad34a59999f29d06ab18e4
Donny <tanadornrach@gmail.com>
2025-08-07 20:46:42 +0700

    alter json data and interfaces schemes

---
+++ src/interfaces/attendee_interface.ts
@@ -0,0 +1,6 @@
+export default interface AttendeeInterface {
+  id: string | number;
+  datetime: string;
+  first_name: string;
+  last_name: string;
+}
--- src/interfaces/bar_chart_interface.ts
+++ src/interfaces/bar_chart_interface.ts
@@ -7,6 +7,7 @@ type barChartDatasetType = {
   label: string,
   data: Array<number>,
   backgroundColor: Array<string> | string,
+  color?: string,
   borderColor?: Array<string> | string,
   borderWidth?: number
 }
