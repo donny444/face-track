1c2f5efcc992b100d08a132887a55e206c4bf6f4
Donny <tanadornrach@gmail.com>
2025-10-15 20:04:36 +0700

    recorrect client dtos

--- src/interfaces/attendee_interface.ts
+++ src/interfaces/attendee_interface.ts
@@ -1,6 +1,8 @@
-export default interface AttendeeInterface {
-  id: string | number;
-  datetime: string;
+export type AttendeesType = Array<AttendeeInterface>;
+
+export interface AttendeeInterface {
+  attendee_id: string;
+  timestamp: number;
   first_name: string;
   last_name: string;
 }
